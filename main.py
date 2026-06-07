06c8218fb60f8572eb69e58ccd12bc165f66f2c0
samuel ordoñez <kaynower@gmail.com>
2026-06-07 17:45:09 -0500

    Changes lenguage & units

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 @app.get('/')
 async def get_data_weather(lat: float = 10.9639, lon: float = -74.7964):
 
-    URL = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHER_KEY}"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&lang=es&appid={WEATHER_KEY}"
 
     async with httpx.AsyncClient() as client:
         response = await client.get(URL)
