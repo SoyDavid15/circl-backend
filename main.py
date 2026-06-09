9f5de3645cbbf47d5e4bf24200c419468df2bd4c
samuel ordoñez <kaynower@gmail.com>
2026-06-09 16:06:07 -0500

    endpoint news has been added

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+import os
 import httpx
+from dotenv import load_dotenv
+load_dotenv()
 
-WEATHER_KEY = "6cc9ccedd6de5691035785e3983f1048"
+WEATHER_KEY = os.getenv("WEATHER_KEY")
 
 app = FastAPI()
+
 
 @app.get('/')
 async def get_data_weather(lat: float = 10.9639, lon: float = -74.7964):
@@ -14,4 +18,10 @@ async def get_data_weather(lat: float = 10.9639, lon: float = -74.7964):
         response = await client.get(URL)
         return response.json()
 
-    
+@app.get('/news')
+async def get_news_data():
+    URL = "https://newsdata.io/api/1/latest?apikey=pub_cae0254cbf7144f7abc4712d69937803&q=barranquilla%20AND%20hurto%20OR%20recompensa%20OR%20ultima%20hora%20OR%20asalto%20OR%20atentado%20OR%20alerta%20OR%20robo&country=co&language=es&category=crime&removeduplicate=1"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+        return response.json()
---
+++ oauth2.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+oauth2 = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.post("/token")
+def login():
+    return "hola"
